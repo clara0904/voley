e0e74bfa363da94b6c41f10837dfb3c950b045c3
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-15 18:39:54 -0300

    refactor: atualização do widget side_column

--- lib/components/side_column.dart
+++ lib/components/side_column.dart
@@ -1,9 +1,11 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:voley_app/components/circle_button.dart';
 import 'list.dart';
 
 class SideColumn extends StatelessWidget {
   final bool isLeft;
-  const SideColumn({super.key, required this.isLeft});
+  final Function onPressed;
+  const SideColumn({super.key, required this.isLeft, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +15,10 @@ class SideColumn extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              List(tipoLance: 'Ace', left: isLeft,),
-              List(tipoLance: 'Ataque', left: isLeft),
-              List(tipoLance: 'Bloqueio', left: isLeft),
-              List(tipoLance: 'Erro', left: isLeft),
+              List(tipoLance: 'Ace', left: isLeft, button: CircleButton(onPressed: onPressed),),
+              List(tipoLance: 'Ataque', left: isLeft, button: CircleButton(onPressed: onPressed),),
+              List(tipoLance: 'Bloqueio', left: isLeft, button: CircleButton(onPressed: onPressed),),
+              List(tipoLance: 'Erro', left: isLeft, button: CircleButton(onPressed: onPressed),),
             ],
           ),
         ),
