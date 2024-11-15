059497deefce592494ad7c244f5db8532ec7080c
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-15 18:39:17 -0300

    refactor: criação de nova classe game_time

---
+++ lib/components/game_time.dart
@@ -0,0 +1,31 @@
+﻿import 'package:flutter/material.dart';
+
+class GameTime extends StatelessWidget {
+  const GameTime({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: const TextSpan(
+        children: [
+          TextSpan(
+            text: "Tempo de jogo: 1:14'",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontFamily: 'ConcertOne',
+            ),
+          ),
+          TextSpan(
+            text: "00''",
+            style: TextStyle(
+              fontSize: 15, 
+              color: Colors.white,
+              fontFamily: 'ConcertOne',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
