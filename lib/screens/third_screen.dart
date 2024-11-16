ef343f4b8ee83fb3d462c0bfc8bcdcb4638d34bf
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-16 16:45:01 -0300

    feat: estilização do widget third_screen

--- lib/screens/third_screen.dart
+++ lib/screens/third_screen.dart
@@ -1,10 +1,63 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:voley_app/components/components_third/results_table.dart';
+import 'package:voley_app/components/components_third/time_line.dart';
+import 'package:voley_app/components/components_third/victories.dart';
 
 class ThirdScreen extends StatelessWidget {
   const ThirdScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+    ]);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xff00ADC3),
+        appBar: AppBar(
+          backgroundColor: const Color(0xff00ADC3), 
+          elevation: 0, 
+          toolbarHeight: 40,
+          centerTitle: true,
+          title: const Text(
+              'PLACAR GERAL',
+              style: TextStyle(
+                color:  Color(0xff2B4A8E),
+                fontSize: 40,
+                fontFamily: 'ConcertOne'
+              ),
+          ),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white, size: 25.0,), 
+            onPressed: () {
+              Navigator.pop(context); 
+            },
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: IconButton(
+                icon: const Icon(Icons.analytics_outlined, color: Colors.white, size: 25.0,), 
+                onPressed: () {
+                  
+                },
+              ),
+            ),
+          ],
+        ),
+        body: const Stack(
+        children: [
+          ResultsTable(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Victories(),
+            ),
+        ],
+      ),
+
+      )
+    );
   }
 }
