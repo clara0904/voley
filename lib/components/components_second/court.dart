8b5f9cefc1051776bbb67ed1d53a2cdea61ecdf5
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-06 16:45:50 -0300

    refactor: adaptação do componente court

--- lib/components/components_second/court.dart
+++ lib/components/components_second/court.dart
@@ -15,66 +15,66 @@ class Court extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      decoration: BoxDecoration(
-        color: const Color(0xffF77859), 
-        border: Border.all(color: Colors.white, width: 3),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                border: Border(
-                  right: BorderSide(color: Colors.white, width: 3), 
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+              color: const Color(0xffF77859), 
+              border: Border.all(color: Colors.white, width: 3),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Visibility(
+                  visible: visibilidadeEsquerda,
+                  child: const CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage('imagens/ball.png'),
+                  ),
                 ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Visibility(
-                    visible: visibilidadeEsquerda,
-                    child: const CircleAvatar(
-                      radius: 25,
-                      backgroundImage: AssetImage('imagens/ball.png'),
-                    ),
+                Text(
+                  scoreLeft,
+                  style: const TextStyle(
+                    fontSize: 70,
+                    color: Colors.white,
                   ),
-                  Text(
-                    scoreLeft,
-                    style: const TextStyle(
-                      fontSize: 70,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          Expanded(
+        ),
+        Expanded(
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+              color: const Color(0xffF77859), 
+              border: Border.all(color: Colors.white, width: 3),
+            ),
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Visibility(
-                    visible: visibilidadeDireita,
-                    child: const CircleAvatar(
-                      radius: 25,
-                      backgroundImage: AssetImage('imagens/ball.png'),
-                    ),
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Visibility(
+                  visible: visibilidadeDireita,
+                  child: const CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage('imagens/ball.png'),
                   ),
-                  Text(
-                    scoreRight,
-                    style: const TextStyle(
-                      fontSize: 70,
-                      color: Colors.white,
-                    ),
+                ),
+                Text(
+                  scoreRight,
+                  style: const TextStyle(
+                    fontSize: 70,
+                    color: Colors.white,
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
