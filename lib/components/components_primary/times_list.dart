ae2e1a2f8581114674708264a266e67506b4919a
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-07 16:19:49 -0300

    feat: personalização do app

--- lib/components/components_primary/times_list.dart
+++ lib/components/components_primary/times_list.dart
@@ -5,41 +5,44 @@ class TimesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: RotatedBox(
-            quarterTurns: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.5),
-                border: Border.all(color: Colors.white, width: 2),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 65),
-              child: const Text(
-                'TIMES',
-                style: TextStyle(
-                  fontSize: 60,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+    return Container(
+      margin: const EdgeInsets.only(top: 80, bottom: 34),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: RotatedBox(
+              quarterTurns: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.5),
+                  border: Border.all(color: Colors.white, width: 2),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 65),
+                child: const Text(
+                  'TIMES',
+                  style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        const Expanded(
-          child: Column(
-            children: [
-              TimesPrimary(time: 'Sicranos', sizetime: '3'),
-              TimesPrimary(time: 'Autoconvidados', sizetime: '3'),
-              TimesPrimary(time: 'Ziraldos', sizetime: '4'),
-              TimesPrimary(time: 'Sparrings', sizetime: '5'),
-            ],
+          const Expanded(
+            child: Column(
+              children: [
+                TimesPrimary(time: 'Atrasados', sizetime: '5'),
+                TimesPrimary(time: 'Penetras', sizetime: '3'),
+                TimesPrimary(time: 'Felizes', sizetime: '4'),
+                TimesPrimary(time: 'Esquecidos', sizetime: '5'),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/components/components_third/victories.dart
+++ lib/components/components_third/victories.dart
@@ -26,11 +26,12 @@ class Victories extends StatelessWidget {
           ),
           ),
           child: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TimeLine(nomeTime: 'Ziraldos',victories: '3',),
-              TimeLine(nomeTime: 'Sicranos',victories: '1',),
-              TimeLine(nomeTime: 'Autoconvidados',victories: '5',),
-              TimeLine(nomeTime: 'Sparrings',victories: '7',),
+              TimeLine(nomeTime: 'Atrasados',victories: '3',),
+              TimeLine(nomeTime: 'Esquecidos',victories: '1',),
+              TimeLine(nomeTime: 'Penetras',victories: '5',),
+              TimeLine(nomeTime: 'Felizes',victories: '7',),
             ],
           )
         ),
