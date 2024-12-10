14916a5e1ec39ac9a51684b8aba655e4d16621ab
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-10 14:05:48 -0300

    refactor: ajuste de responsividade na primeira tela

--- lib/screens/primary_screen.dart
+++ lib/screens/primary_screen.dart
@@ -12,9 +12,6 @@ class PrimaryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
 
     return SafeArea(
       child: Scaffold(
